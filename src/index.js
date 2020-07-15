391a34b386cac1d437af85dba59867b5d4b93c98
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-15 15:21:31 +0200

    eslint

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>, document.getElementById('root')
 );
 
 serviceWorker.unregister();
