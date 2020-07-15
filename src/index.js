2e2402e5bd07cd5a61f5a71bf89b74ae3e580717
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-15 15:59:25 +0200

    tralling coma

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>, document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 serviceWorker.unregister();
