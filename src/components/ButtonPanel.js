1d412d7bd2b7931ba713828223f07a9051eef630
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-16 19:57:29 +0200

    .

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import Button from './Button';
+import PropTypes from 'prop-types';
 
-const ButtonPanel = () => (
-  <div className='buttonPanel'>
-    <div className='group'>
-      <Button name='AC' color='lightgrey' />
-      <Button name='+/-' color='lightgrey' />
-      <Button name='%' color='lightgrey' />
-      <Button name='/' />
-    </div>
-    <div className='group'>
-      <Button name='7' color='lightgrey' />
-      <Button name='8' color='lightgrey' />
-      <Button name='9' color='lightgrey' />
-      <Button name='X' />
-    </div>
-    <div className='group'>
-      <Button name='4' color='lightgrey' />
-      <Button name='5' color='lightgrey' />
-      <Button name='6' color='lightgrey' />
-      <Button name='-' />
-    </div>
-    <div className='group'>
-      <Button name='1' color='lightgrey' />
-      <Button name='2' color='lightgrey' />
-      <Button name='3' color='lightgrey' />
-      <Button name='+' />
-    </div>
-    <div className='group'>
-      <Button name='0' color='lightgrey' wide />
-      <Button name='.' color='lightgrey' />
-      <Button name='=' />
-    </div>
-  </div>
-);
+class ButtonPanel extends React.Component {
+  render() {
+    return (
+      <div className='buttonPanel'>
+        <div className='group'>
+          <Button onClick={name => this.props.onClick(name)} name='AC' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='+/-' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='%' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='/' />
+        </div>
+        <div className='group'>
+          <Button onClick={name => this.props.onClick(name)} name='7' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='8' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='9' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='X' />
+        </div>
+        <div className='group'>
+          <Button onClick={name => this.props.onClick(name)} name='4' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='5' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='6' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='-' />
+        </div>
+        <div className='group'>
+          <Button onClick={name => this.props.onClick(name)} name='1' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='2' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='3' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='+' />
+        </div>
+        <div className='group'>
+          <Button onClick={name => this.props.onClick(name)} name='0' color='lightgrey' wide />
+          <Button onClick={name => this.props.onClick(name)} name='.' color='lightgrey' />
+          <Button onClick={name => this.props.onClick(name)} name='=' />
+        </div>
+      </div>
+    );
+  }
+}
+ButtonPanel.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
